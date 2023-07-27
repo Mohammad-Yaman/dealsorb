d1bf0ccda5090994c69d30c5a17cd3f1afc84416
Mohammad-Yaman <140715791+Mohammad-Yaman@users.noreply.github.com>
2023-07-27 16:54:39 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      display: ['group-focus'],
+      opacity: ['group-focus'],
+      inset: ['group-focus']
+    },
+  },
+  plugins: [],
+}
+
